fe5880ec3c1ef32bc902452b9b9d2cb9900cd1f2
mikimo <mikimo@tencent.com>
2024-07-06 17:39:06 +0800

    Fix: fixed build

--- androidApp/src/main/java/com/example/mokmmdemo/android/TimerDemo.kt
+++ androidApp/src/main/java/com/example/mokmmdemo/android/TimerDemo.kt
@@ -1,30 +1,65 @@
-package com.tencent.tmm.qmt.common.com.example.mokmmdemo.android
+package com.example.mokmmdemo.android
 
+import android.content.res.Resources
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.shape.GenericShape
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.mokmmdemo.Greeting
-import com.example.mokmmdemo.android.MyApplicationTheme
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+
+class TimerDemo : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Row(
+                        modifier = Modifier
+                            .padding(all = 30.dp)
+                            .fillMaxWidth()
+                            .height(44.dp),
+                        horizontalArrangement = Arrangement.SpaceEvenly,
+                    ) {
+                        Text(text = "开始计时", modifier = Modifier.clickable {
+                            println("momo: click start")
+                            timer.start(interval = 2000)
+                        })
+                        Text(text = "结束计时", modifier = Modifier.clickable {
+                            println("momo: click stop")
+                            timer.stop()
+                        })
+                    }
+                }
+            }
+        }
+    }
+    private val timer = OnceCommonTimer {
+        println("momo: timer out")
+    }
+}
 
 open class OnceCommonTimer(private val timeOutCallback: () -> Unit) {
     fun start(interval: Long) {
@@ -90,31 +125,4 @@ open class CommonTimer(private val timeOutCallback: (repeatIdex: Int) -> Unit) {
 
     private var job: Job? = null
     private var stoped: Boolean = true
-}
-
-class TimerDemo : ComponentActivity() {
-    private val timer = OnceCommonTimer {
-        println("momo: timer out")
-    }
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            Row(
-                modifier = Modifier
-                    .padding(all = 30.dp)
-                    .fillMaxWidth()
-                    .height(44.dp),
-                horizontalArrangement = Arrangement.SpaceEvenly,
-            ) {
-                Text(text = "开始计时", modifier = Modifier.clickable {
-                    println("momo: click start")
-                    timer.start(interval = 2000)
-                })
-                Text(text = "结束计时", modifier = Modifier.clickable {
-                    println("momo: click stop")
-                    timer.stop()
-                })
-            }
-        }
-    }
 }
