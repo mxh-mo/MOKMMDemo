825058ac5ccd385f410587cf34e3b025cce6aa24
mikimo <mikimo@tencent.com>
2024-08-11 13:10:45 +0800

    add: state demo

--- androidApp/src/main/java/com/example/mokmmdemo/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/mokmmdemo/android/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.mokmmdemo.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.mokmmdemo.Greeting
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +26,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingView(Greeting().greet())
+//                    GreetingView(Greeting().greet())
+                    WellnessScreen()
                 }
             }
         }
